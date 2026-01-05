25e35066c0a023c9846630b84e45e879c822cff9
sudheera2003 <sudheeradilum@gmail.com>
2026-01-05 20:29:00 +0530

    hotfix

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -163,15 +163,6 @@ const Calendar = React.forwardRef<HTMLDivElement, CalendarProps>(
             <button className="px-3 py-2 text-sm font-medium border-b-2 border-blue-500 text-blue-600">
               All events
             </button>
-            <button className="px-3 py-2 text-sm text-gray-500 hover:text-gray-700">
-              Shared
-            </button>
-            <button className="px-3 py-2 text-sm text-gray-500 hover:text-gray-700">
-              Public
-            </button>
-            <button className="px-3 py-2 text-sm text-gray-500 hover:text-gray-700">
-              Archived
-            </button>
           </div>
 
           {/* Calendar Grid */}
